1bd49ac36f5fa0b27c831bac69530ab843c890f3
Toni Uebernickel <tuebernickel@gmail.com>
2011-11-23 15:55:20 +0100

    add vendor/bundles as namespace fallback

--- app/autoload.php
+++ app/autoload.php
@@ -6,8 +6,6 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
     'Symfony'          => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
-    'Sensio'           => __DIR__.'/../vendor/bundles',
-    'JMS'              => __DIR__.'/../vendor/bundles',
     'Doctrine\\Common' => __DIR__.'/../vendor/doctrine-common/lib',
     'Doctrine\\DBAL'   => __DIR__.'/../vendor/doctrine-dbal/lib',
     'Doctrine'         => __DIR__.'/../vendor/doctrine/lib',
@@ -29,6 +27,7 @@ if (!function_exists('intl_get_error_code')) {
 
 $loader->registerNamespaceFallbacks(array(
     __DIR__.'/../src',
+    __DIR__.'/../vendor/bundles',
 ));
 $loader->register();
 
