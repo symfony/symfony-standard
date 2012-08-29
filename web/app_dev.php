f094fb14ce1e0e80833eef11da17e4ace0042ad6
Brikou Carré <brikou@gmail.com>
2012-08-29 08:45:49 +0200

    added missing umask comment in web/app.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,10 +1,10 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
-
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
+
+use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
