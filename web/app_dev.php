605165ed6e8598d9a696dccddc286de2d9861ccc
Bilal Amarni <bilal.amarni@gmail.com>
2013-01-10 10:31:22 +0100

    switched to autoload.php in the dev front controller

--- web/app_dev.php
+++ web/app_dev.php
@@ -17,7 +17,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
+require_once __DIR__.'/../app/autoload.php';
 require_once __DIR__.'/../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
