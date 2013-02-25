a166bc0e5731c2c97017197949a68e70505fff24
Tobias Schultze <webmaster@tubo-world.de>
2013-02-25 18:13:31 +0100

    use require_once instead of require

--- app/autoload.php
+++ app/autoload.php
@@ -2,7 +2,7 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-$loader = require __DIR__.'/../vendor/autoload.php';
+$loader = require_once __DIR__.'/../vendor/autoload.php';
 
 // intl
 if (!function_exists('intl_get_error_code')) {
