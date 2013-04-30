043b3d174b35ffe1d5ccff3d23daa9521749ee7a
Sebastiaan Stok <s.stok@rollerscapes.net>
2013-04-30 15:27:39 +0300

    Update autoload.php
    Stubs have moved from Locale to Intl

--- app/autoload.php
+++ app/autoload.php
@@ -10,7 +10,7 @@ $loader = require __DIR__.'/../vendor/autoload.php';
 
 // intl
 if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
+    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Intl/Resources/stubs/functions.php';
 }
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
