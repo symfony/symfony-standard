a65f9acaf687ea446991f6fe9396246fc75de354
WouterJ <waldio.webdesign@gmail.com>
2014-03-03 20:05:56 +0100

    Added bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,8 +21,12 @@ class AppKernel extends Kernel
             // Symfony CMF Standard Edition Bundles
             new Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle(),
             new Symfony\Cmf\Bundle\CoreBundle\CmfCoreBundle(),
+            new Symfony\Cmf\Bundle\ContentBundle\CmfContentBundle(),
             new Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
             new Symfony\Cmf\Bundle\SimpleCmsBundle\CmfSimpleCmsBundle(),
+
+            new Symfony\Cmf\Bundle\BlockBundle\CmfBlockBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
 
             new Symfony\Cmf\Bundle\MenuBundle\CmfMenuBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
