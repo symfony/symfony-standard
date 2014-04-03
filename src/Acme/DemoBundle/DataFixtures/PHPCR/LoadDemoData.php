715a2cf6a48f6cd621e81d118394dd442cee580f
WouterJ <waldio.webdesign@gmail.com>
2014-04-03 21:39:34 +0200

    Added Home to menu

--- src/Acme/DemoBundle/DataFixtures/PHPCR/LoadDemoData.php
+++ src/Acme/DemoBundle/DataFixtures/PHPCR/LoadDemoData.php
@@ -6,6 +6,7 @@ use Nelmio\Alice\Fixtures;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Symfony\Cmf\Bundle\SimpleCmsBundle\Doctrine\Phpcr\Page;
+use Symfony\Cmf\Bundle\MenuBundle\Doctrine\Phpcr\MenuNode;
 
 /**
  * Loads the initial demo data of the demo website.
@@ -19,16 +20,27 @@ class LoadDemoData implements FixtureInterface
      */
     public function load(ObjectManager $manager)
     {
+        // tweak homepage
+        $page = $manager->find(null, '/cms/simple');
+        $page->setBody('Hello');
+        $page->setDefault('_template', 'AcmeDemoBundle::home.html.twig');
+
+        // add menu item for home
+        $menuRoot = $manager->find(null, '/cms/simple');
+        $homeMenuNode = new MenuNode('home');
+        $homeMenuNode->setLabel('Home');
+        $homeMenuNode->setParent($menuRoot);
+        $homeMenuNode->setContent($page);
+
+        $manager->persist($homeMenuNode);
+
         // load the yaml file in src/Acme/DemoBundle/Resources/data/pages.yml
         Fixtures::load(array(__DIR__.'/../../Resources/data/pages.yml'), $manager);
 
         // load the blocks
         Fixtures::load(array(__DIR__.'/../../Resources/data/blocks.yml'), $manager);
 
-        $page = $manager->find(null, '/cms/simple');
-        $page->setBody('Hello');
-        $page->setDefault('_template', 'AcmeDemoBundle::home.html.twig');
-
+        // save the changes
         $manager->flush();
     }
 }
