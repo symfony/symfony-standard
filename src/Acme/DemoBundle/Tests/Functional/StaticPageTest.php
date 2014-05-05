0f215f5bfc5a1b078dadb73e2f1d651099f8364d
WouterJ <waldio.webdesign@gmail.com>
2014-05-05 20:41:25 +0200

    Fixed tests

--- src/Acme/DemoBundle/Tests/Functional/StaticPageTest.php
+++ src/Acme/DemoBundle/Tests/Functional/StaticPageTest.php
@@ -30,7 +30,9 @@ class StaticPageTest extends WebTestCase
         $title = 'CMF Demo';
         $this->assertCount(1, $crawler->filter(sprintf('h1:contains("%s")', $title)), 'Page does not contain an h1 tag with: '.$title);
 
+
         $body = 'Hello! This page is created by the Symfony CMF. You can edit this page if you are logged in as admin.';
-        $this->assertCount(1, $crawler->filter(sprintf('p:contains("%s")', $body)), 'Page does not contain an p tag with: '.$body);
+        $this->assertCount(1, $crawler->filter(sprintf('body:contains("%s")', $body)), 'Page does not contain the text: '.$body);
     }
 }
+
