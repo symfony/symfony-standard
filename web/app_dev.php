66019d08389def9325185e3f9e097d7d08482043
AlexanderC <self@alexanderc.me>
2014-08-26 18:22:49 +0300

    Fix OPTION request issue for development env
    Fix OPTION request issue for development environment when trying to request backend from another host(or even port). [CORS fix]

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,6 +1,8 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\HeaderBag;
 use Symfony\Component\Debug\Debug;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
@@ -11,10 +13,31 @@ use Symfony\Component\Debug\Debug;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+} elseif('OPTIONS' === $_SERVER['REQUEST_METHOD']) {
+    require __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/HttpFoundation/HeaderBag.php';
+    require __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/HttpFoundation/Response.php';
+    require __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/HttpFoundation/ResponseHeaderBag.php';
+
+    $headerBag = new HeaderBag(getallheaders());
+    $responseHeaders = array();
+
+    foreach(array(
+                'Access-Control-Allow-Origin' => 'Origin',
+                'Access-Control-Allow-Methods' => 'Access-Control-Request-Method',
+                'Access-Control-Allow-Headers' => 'Access-Control-Request-Headers',
+                'Access-Control-Allow-Credentials' => 'Access-Control-Allow-Credentials'
+            ) as $header => $headerKey) {
+        $responseHeaders[$header] = $headerBag->get($headerKey);
+    }
+
+    $response = new Response(null, Response::HTTP_NO_CONTENT, $responseHeaders);
+
+    $response->send();
+    exit;
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
