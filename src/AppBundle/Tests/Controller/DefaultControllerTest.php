6288bcd1fdf74510a51df547ceab8032e5c76f19
Javier Eguiluz <javier.eguiluz@gmail.com>
2015-07-22 22:14:38 +0200

    Refactored the sample functional test

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -13,6 +13,6 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $client->getResponse()->getContent());
+        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
 }
