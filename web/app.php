ad8c05a1a32d8fd5788e7f9d420fbfba178a8896
prestataire <jpasqualini75@gmail.com>
2016-03-14 15:16:19 +0100

    issue-18160 : reproducing cases /app.php/casA (cas OK) , /app.php/casB (cas KO)

--- web/app.php
+++ web/app.php
@@ -14,11 +14,11 @@ $loader->register(true);
 */
 
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
