36c51d81337f467543a3579d51e37bb1f363419a
Kévin Dunglas <dunglas@gmail.com>
2016-03-20 21:54:26 +0100

    Support for controller autoregistration and autowiring

--- src/AppBundle/Action/DefaultAction.php
+++ src/AppBundle/Action/DefaultAction.php
@@ -1,17 +1,20 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace AppBundle\Action;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\ControllerTrait;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class DefaultAction extends Controller
 {
+    use ControllerTrait;
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function __invoke(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
