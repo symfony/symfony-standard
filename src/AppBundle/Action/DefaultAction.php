e97c2ca4577580cdb374914231c2759a6a4974f6
Kévin Dunglas <dunglas@gmail.com>
2016-03-20 22:29:20 +0100

    Don't extend Controller

--- src/AppBundle/Action/DefaultAction.php
+++ src/AppBundle/Action/DefaultAction.php
@@ -3,13 +3,20 @@
 namespace AppBundle\Action;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\ControllerTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultAction extends Controller
+class DefaultAction
 {
     use ControllerTrait;
+
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
 
     /**
      * @Route("/", name="homepage")
@@ -18,7 +25,7 @@ class DefaultAction extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ]);
     }
 }
