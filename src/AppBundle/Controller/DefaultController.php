f25a6b90001f6fef8eb2b5363e0c22ab94ab508f
Kévin Dunglas <dunglas@gmail.com>
2016-03-21 09:10:31 +0100

    Integrate DunglasActionBundle

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,17 +2,27 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\MyService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    private $myService;
+
+    public function __construct(MyService $myService) // to be removed, just to show advantages of this system
+    {
+        $this->myService = $myService;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
+        $this->myService->sayHello($request->query->get('name', 'Kévin'));
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
