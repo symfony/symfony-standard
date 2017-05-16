66f580449c29a351d50a73eb233a689fe265fa71
Dawid Nowak <code@dnowak.pl>
2017-05-16 20:18:07 +0200

    app.php and console: no need to declare $loader variable
    The var isn't being used anywhere in the file(s).
    
    Because of how "require" works,
    $loader is still a global variable.

--- web/app.php
+++ web/app.php
@@ -2,8 +2,7 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-
+require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
