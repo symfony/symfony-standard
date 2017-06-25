bef989fdb8ccd257ed3674f96ab0a5b57f07ae8c
Vaibhav Pandey <contact@vaibhavpandey.com>
2017-06-25 08:24:54 +0530

    integrate @symfony/webpack-encore into the app

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+// directory where all compiled assets will be stored
+    .setOutputPath('web/build/')
+
+    // what's the public path to this directory (relative to your project's document root dir)
+    .setPublicPath('/build')
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // will output as web/build/app.js
+    .addEntry('app', './assets/js/main.js')
+
+    // will output as web/build/global.css
+    .addStyleEntry('global', './assets/css/global.scss')
+
+    // allow sass/scss files to be processed
+    .enableSassLoader()
+
+    // allow legacy applications to use $/jQuery as a global variable
+    // .autoProvidejQuery()
+
+    .enableSourceMaps(!Encore.isProduction())
+
+// create hashed filenames (e.g. app.abc123.css)
+// .enableVersioning()
+;
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig();
