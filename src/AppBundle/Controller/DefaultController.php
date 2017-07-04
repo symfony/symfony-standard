c4c2ef5d383fd0a39adbb1eb5c8840a412fd4335
Fabien Potencier <fabien.potencier@gmail.com>
2017-07-04 09:28:41 +0300

    Revert "feature #1082 DefaultController extends the new AbstractController (vudaltsov)"
    This reverts commit 2e8d74c2900148c9e1c51fe2b0662f74af882e8f, reversing
    changes made to fc93b84d0a969907104b278351143f2312f3c9e4.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,10 +3,10 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends AbstractController
+class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
